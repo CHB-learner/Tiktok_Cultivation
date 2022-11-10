a5cf28174e3119ee1f8441c906c73066e3e64760
CHB-learner <104660431+CHB-learner@users.noreply.github.com>
2022-11-10 01:19:43 +0000

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,267 @@
+import os
+import json
+import os
+import json
+
+import torch
+from PIL import Image
+from torchvision import transforms
+
+
+import torch
+from PIL import Image
+from torchvision import transforms
+
+import torch.nn as nn
+import torch
+
+
+# 定义18,34残差结构
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
+                               kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel,
+                               kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+# 定义50,101,152残差结构
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None,
+                 groups=1, width_per_group=64):
+        super(Bottleneck, self).__init__()
+
+        width = int(out_channel * (width_per_group / 64.)) * groups
+
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width,
+                               kernel_size=1, stride=1, bias=False)  # squeeze channels
+        self.bn1 = nn.BatchNorm2d(width)
+        # -----------------------------------------
+        self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups,
+                               kernel_size=3, stride=stride, bias=False, padding=1)
+        self.bn2 = nn.BatchNorm2d(width)
+        # -----------------------------------------
+        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel * self.expansion,
+                               kernel_size=1, stride=1, bias=False)  # unsqueeze channels
+        self.bn3 = nn.BatchNorm2d(out_channel * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+# ----------------------------------------------------------------------------------------------------
+class ResNet(nn.Module):
+
+    def __init__(self,
+                 block,
+                 blocks_num,
+                 num_classes=1000,
+                 include_top=True,
+                 groups=1,
+                 width_per_group=64):
+        super(ResNet, self).__init__()
+        self.include_top = include_top
+        self.in_channel = 64
+
+        self.groups = groups
+        self.width_per_group = width_per_group
+        # 7X7卷积
+        self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2,
+                               padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)
+        # 最大池化
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # conv2_x
+        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+        # conv3_x
+        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
+        # conv4_x
+        self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
+        # conv5_x
+        self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
+
+        if self.include_top:
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # output size = (1, 1)
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def _make_layer(self, block, channel, block_num, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channel != channel * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channel, channel * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(channel * block.expansion))
+
+        layers = []
+        layers.append(block(self.in_channel,
+                            channel,
+                            downsample=downsample,
+                            stride=stride,
+                            groups=self.groups,
+                            width_per_group=self.width_per_group))
+        self.in_channel = channel * block.expansion
+
+        for _ in range(1, block_num):
+            layers.append(block(self.in_channel,
+                                channel,
+                                groups=self.groups,
+                                width_per_group=self.width_per_group))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        if self.include_top:
+            x = self.avgpool(x)
+            x = torch.flatten(x, 1)
+            x = self.fc(x)
+
+        return x
+
+def resnext101_32x8d(num_classes=1000, include_top=True):
+    groups = 32
+    width_per_group = 8
+    return ResNet(Bottleneck, [3, 4, 23, 3],
+                  num_classes=num_classes,
+                  include_top=include_top,
+                  groups=groups,
+                  width_per_group=width_per_group)
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+data_transform = transforms.Compose(
+    [transforms.Resize(256),
+     transforms.CenterCrop(224),
+     transforms.ToTensor(),
+     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------
+import pyautogui
+import time
+
+import datetime
+import os
+
+n = 0
+like = 1
+SUM = 0
+while True:
+
+    time.sleep(4.0)
+    # load image
+    video_IMG= pyautogui.screenshot(region=(50, 130, 1700 ,770))
+
+    img = video_IMG.convert('RGB')
+    img = data_transform(img)
+    img = torch.unsqueeze(img, dim=0)
+    # read class_indict
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+    with open(json_path, "r") as f:
+        class_indict = json.load(f)
+    # create model
+    model = resnext101_32x8d(num_classes=2).to(device)
+    # load model weights
+    weights_path = "./PTH___PTH.pth"
+    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
+    model.load_state_dict(torch.load(weights_path, map_location=device))
+    # prediction
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+    SEX = class_indict[str(predict_cla)]
+    GRADE = predict[predict_cla].numpy()
+
+
+
+    #print(SEX,GRADE)
+    if SEX == 'girl' and GRADE >= 0.65:
+        SUM = SUM + GRADE
+        like = like + 1
+        print("小姐姐 点赞  啦!!!!!!")
+
+        img_number = len(os.listdir("./nice_NICE"))
+
+        img_path = './nice_NICE/{}.png'.format(img_number)
+        video_IMG.save(img_path)
+
+
+        time.sleep(0.1)
+        pyautogui.press('space')
+        time.sleep(0.1)
+        pyautogui.press('space')
+
+        time.sleep(0.4)
+        pyautogui.press('down')
+    else:
+        print("不喜欢 划过")
+        pyautogui.press('down')
+    print("今日共刷:" , n , "已点赞小姐姐:" , like ,"均分:" , SUM/like)
+    print()
+    n = n+1
+
+
+
---
+++ resnext101_32x8d.py
@@ -0,0 +1,340 @@
+import torch.nn as nn
+import torch
+
+
+# 定义18,34残差结构
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
+                               kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel,
+                               kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+# 定义50,101,152残差结构
+class Bottleneck(nn.Module):
+    """
+    注意：原论文中，在虚线残差结构的主分支上，第一个1x1卷积层的步距是2，第二个3x3卷积层步距是1。
+    但在pytorch官方实现过程中是第一个1x1卷积层的步距是1，第二个3x3卷积层步距是2，
+    这么做的好处是能够在top1上提升大概0.5%的准确率。
+    可参考Resnet v1.5 https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch
+    """
+    expansion = 4
+
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None,
+                 groups=1, width_per_group=64):
+        super(Bottleneck, self).__init__()
+
+        width = int(out_channel * (width_per_group / 64.)) * groups
+
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width,
+                               kernel_size=1, stride=1, bias=False)  # squeeze channels
+        self.bn1 = nn.BatchNorm2d(width)
+        # -----------------------------------------
+        self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups,
+                               kernel_size=3, stride=stride, bias=False, padding=1)
+        self.bn2 = nn.BatchNorm2d(width)
+        # -----------------------------------------
+        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel * self.expansion,
+                               kernel_size=1, stride=1, bias=False)  # unsqueeze channels
+        self.bn3 = nn.BatchNorm2d(out_channel * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+# ----------------------------------------------------------------------------------------------------
+class ResNet(nn.Module):
+
+    def __init__(self,
+                 block,
+                 blocks_num,
+                 num_classes=1000,
+                 include_top=True,
+                 groups=1,
+                 width_per_group=64):
+        super(ResNet, self).__init__()
+        self.include_top = include_top
+        self.in_channel = 64
+
+        self.groups = groups
+        self.width_per_group = width_per_group
+        # 7X7卷积
+        self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2,
+                               padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)
+        # 最大池化
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # conv2_x
+        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+        # conv3_x
+        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
+        # conv4_x
+        self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
+        # conv5_x
+        self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
+
+        if self.include_top:
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # output size = (1, 1)
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def _make_layer(self, block, channel, block_num, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channel != channel * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channel, channel * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(channel * block.expansion))
+
+        layers = []
+        layers.append(block(self.in_channel,
+                            channel,
+                            downsample=downsample,
+                            stride=stride,
+                            groups=self.groups,
+                            width_per_group=self.width_per_group))
+        self.in_channel = channel * block.expansion
+
+        for _ in range(1, block_num):
+            layers.append(block(self.in_channel,
+                                channel,
+                                groups=self.groups,
+                                width_per_group=self.width_per_group))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        if self.include_top:
+            x = self.avgpool(x)
+            x = torch.flatten(x, 1)
+            x = self.fc(x)
+
+        return x
+
+def resnext101_32x8d(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnext101_32x8d-8ba56ff5.pth
+    groups = 32
+    width_per_group = 8
+    return ResNet(Bottleneck, [3, 4, 23, 3],
+                  num_classes=num_classes,
+                  include_top=include_top,
+                  groups=groups,
+                  width_per_group=width_per_group)
+
+
+
+
+import os
+import sys
+import json
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms, datasets
+from tqdm import tqdm
+import time
+
+start=time.perf_counter()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("using {} device.".format(device))
+
+data_transform = {
+    "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                 transforms.RandomHorizontalFlip(),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+    "val": transforms.Compose([transforms.Resize(256),
+                               transforms.CenterCrop(224),
+                               transforms.ToTensor(),
+                               transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
+
+image_path =  "../../data/AAAA"
+
+train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
+                                     transform=data_transform["train"])
+train_num = len(train_dataset)
+
+
+
+
+flower_list = train_dataset.class_to_idx
+cla_dict = dict((val, key) for key, val in flower_list.items())
+# write dict into json file
+json_str = json.dumps(cla_dict, indent=4)
+with open('class_indices.json', 'w') as json_file:
+    json_file.write(json_str)
+
+
+
+batch_size = 64
+
+
+train_loader = torch.utils.data.DataLoader(train_dataset,
+                                           batch_size=batch_size, shuffle=True,
+                                           num_workers=0)
+
+validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
+                                        transform=data_transform["val"])
+val_num = len(validate_dataset)
+validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                              batch_size=batch_size, shuffle=False,
+                                              num_workers=0)
+print("using {} images for training, {} images for validation.".format(train_num,
+                                                                       val_num))
+
+net = resnext101_32x8d()#不传入参数，采取迁移学习
+
+
+#____________________迁移学习_____________________________
+# load pretrain weights
+# download url: https://download.pytorch.org/models/resnet34-333f7ec4.pth
+model_weight_path = "./resnext101_32x8d.pth"
+assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
+
+net.load_state_dict(torch.load(model_weight_path, map_location='cpu'))
+
+#冻结所有权重，只训练全连接
+# for param in net.parameters():
+#     param.requires_grad = False
+
+# change fc layer structure
+in_channel = net.fc.in_features
+net.fc = nn.Linear(in_channel, 2) #1000--->5
+net.to(device)
+
+#----------------------------------------------------------
+
+# define loss function
+loss_function = nn.CrossEntropyLoss()
+
+# construct an optimizer
+params = [p for p in net.parameters() if p.requires_grad]
+optimizer = optim.Adam(params, lr=0.0001)
+
+epochs = 100
+best_acc = 0.0
+save_path = './PTH___PTH.pth'
+train_steps = len(train_loader)
+for epoch in range(epochs):
+    # train
+    net.train()
+    running_loss = 0.0
+    train_bar = tqdm(train_loader, file=sys.stdout)
+    for step, data in enumerate(train_bar):
+        images, labels = data
+        optimizer.zero_grad()
+        logits = net(images.to(device))
+        loss = loss_function(logits, labels.to(device))
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+
+        train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
+                                                                 epochs,
+                                                                 loss)
+
+    # validate
+    net.eval()
+    acc = 0.0  # accumulate accurate number / epoch
+    with torch.no_grad():
+        val_bar = tqdm(validate_loader, file=sys.stdout)
+        for val_data in val_bar:
+            val_images, val_labels = val_data
+            outputs = net(val_images.to(device))
+            # loss = loss_function(outputs, test_labels)
+            predict_y = torch.max(outputs, dim=1)[1]
+            acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+            val_bar.desc = "valid epoch[{}/{}]".format(epoch + 1,
+                                                       epochs)
+
+    val_accurate = acc / val_num
+    print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
+          (epoch + 1, running_loss / train_steps, val_accurate))
+
+    if val_accurate > best_acc:
+        best_acc = val_accurate
+        torch.save(net.state_dict(), save_path)
+
+print ('Finished Training')
+end = time.perf_counter()
+print ((end-start)/60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
